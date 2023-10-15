872058dabd502f12230a7a58ce66d3633450e57f
Notsoepicgamer17 <86175993+Notsoepicgamer17@users.noreply.github.com>
2023-10-14 23:29:33 -0400

    Added type hints

--- project_generator.py
+++ project_generator.py
@@ -3,15 +3,15 @@ import shutil
 
 class ProjectGenerator:
     @staticmethod
-    def get_project_directory(parent_directory, project_name):
+    def get_project_directory(parent_directory: str, project_name: str) -> str:
         return os.path.join(parent_directory, project_name)
 
     @staticmethod
-    def create_project_directory(parent_directory, project_name):
+    def create_project_directory(parent_directory: str, project_name: str) -> None:
         os.mkdir(ProjectGenerator.get_project_directory(parent_directory, project_name))
 
     @staticmethod
-    def copy_project_contents(source_folder, parent_directory, project_name):
+    def copy_project_contents(source_folder: str, parent_directory: str, project_name: str) -> None:
         for item_name in os.listdir(source_folder):
             source = os.path.join(source_folder, item_name)
             destination = os.path.join(ProjectGenerator.get_project_directory(parent_directory, project_name), item_name)
@@ -25,7 +25,7 @@ class ProjectGenerator:
                 print('copied', item_name, 'and its contents')
 
     @staticmethod
-    def create_web_development_project(parent_directory, project_name):
+    def create_web_development_project(parent_directory: str, project_name: str) -> None:
         source_folder = r"./projects/web development"
         ProjectGenerator.create_project_directory(parent_directory, project_name)
         ProjectGenerator.copy_project_contents(source_folder, parent_directory, project_name)
