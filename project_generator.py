d5aeb3bf3eaee53b9835d60ffde114d89d369939
Notsoepicgamer17 <86175993+Notsoepicgamer17@users.noreply.github.com>
2023-10-14 21:33:55 -0400

    Abstract functions to class

---
+++ project_generator.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+
+class ProjectGenerator:
+    @staticmethod
+    def get_project_directory(parent_directory, project_name):
+        return os.path.join(parent_directory, project_name)
+
+    @staticmethod
+    def create_project_directory(parent_directory, project_name):
+        os.mkdir(ProjectGenerator.get_project_directory(parent_directory, project_name))
+
+    @staticmethod
+    def copy_project_contents(source_folder, parent_directory, project_name):
+        for item_name in os.listdir(source_folder):
+            source = os.path.join(source_folder, item_name)
+            destination = os.path.join(ProjectGenerator.get_project_directory(parent_directory, project_name), item_name)
+
+            if os.path.isfile(source):
+                shutil.copy(source, destination)
+                print('copied', item_name)
+
+            elif os.path.isdir(source):
+                shutil.copytree(source, destination, symlinks=False, ignore=None)
+                print('copied', item_name, 'and its contents')
+
+    @staticmethod
+    def create_web_development_project(parent_directory, project_name):
+        source_folder = r"./projects/web development"
+        ProjectGenerator.create_project_directory(parent_directory, project_name)
+        ProjectGenerator.copy_project_contents(source_folder, parent_directory, project_name)
